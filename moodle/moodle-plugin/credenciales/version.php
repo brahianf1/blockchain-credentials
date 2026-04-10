fa6c2964cf1380dca4143f5c7eb4dc0658fec78e
Brahian Flores <floresbrahian@gmail.com>
2026-04-10 18:57:41 -0300

    chore(moodle): bump plugin version to force cache upgrade
    - Bump plugin version to 2026041000 in version.php
    - Force Moodle to reload applicable_formats registry and AMD javascript caches dynamically upon upgrade notification

--- moodle/moodle-plugin/credenciales/version.php
+++ moodle/moodle-plugin/credenciales/version.php
@@ -1,5 +1,5 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'block_credenciales';
-$plugin->version   = 2024080201; // YYYYMMDDXX
+$plugin->version   = 2026041000; // Force upgrade to reload applicable_formats and AMD cache
 $plugin->requires  = 2021112900; // Moodle 4.0
