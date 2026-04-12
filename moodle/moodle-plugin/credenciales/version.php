f586694b7498d640b971bccb2e68578e350345ba
Brahian Flores <floresbrahian@gmail.com>
2026-04-12 18:03:52 -0300

    chore(moodle): bump plugin version to trigger cache and db refresh

--- moodle/moodle-plugin/credenciales/version.php
+++ moodle/moodle-plugin/credenciales/version.php
@@ -1,5 +1,5 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'block_credenciales';
-$plugin->version   = 2026041000; // Force upgrade to reload applicable_formats and AMD cache
+$plugin->version   = 2026041200; // Bumped version to force cache/DB refresh for MD3 redesign
 $plugin->requires  = 2021112900; // Moodle 4.0
