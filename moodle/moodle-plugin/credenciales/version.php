120f463e91ce32c9a2eb6801808ed1af9df63b3e
Brahian Flores <floresbrahian@gmail.com>
2026-06-24 18:24:21 -0300

    chore(moodle-plugin): bump version to refresh template and lib caches

--- moodle/moodle-plugin/credenciales/version.php
+++ moodle/moodle-plugin/credenciales/version.php
@@ -1,5 +1,5 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'block_credenciales';
-$plugin->version   = 2026042300; // Admin role via JWT + portal link for all users
+$plugin->version   = 2026062400; // Diploma QR + share button point to real public verification URL
 $plugin->requires  = 2021112900; // Moodle 4.0
